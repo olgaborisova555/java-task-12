6c16cde6530fec85d4838b31dd663bb8ac8b1c67
olgaborisova555 <olga.borisova803@gmail.com>
2020-11-26 01:48:16 +0300

    code reformated

--- src/main/java/ru/netology/stats/Statistics.java
+++ src/main/java/ru/netology/stats/Statistics.java
@@ -2,54 +2,59 @@ package ru.netology.stats;
 
 public class Statistics {
 
-    public int calculateSum(int[] data){
+    public int calculateSum(int[] data) {
         int sum = 0;
-        for (int value : data){
+        for (int value : data) {
             sum = sum + value;
         }
         return sum;
     }
-    public float calculateMean(int[] data){
+
+    public float calculateMean(int[] data) {
         int sum = calculateSum(data);
-        return (float) sum/12;
+        return (float) sum / 12;
     }
-    public int calculateMin(int[] data){
+
+    public int calculateMin(int[] data) {
         int min = data[0];
         int month = 1;
-        for (int i=1; i < data.length; i++){
-            if (data[i] < min){
+        for (int i = 1; i < data.length; i++) {
+            if (data[i] < min) {
                 min = data[i];
-                month = i+1;
+                month = i + 1;
             }
         }
         return month;
     }
-    public int calculateMax(int[] data){
+
+    public int calculateMax(int[] data) {
         int max = data[0];
         int month = 1;
-        for (int i=1; i > data.length; i++){
-            if (data[i] > max){
+        for (int i = 1; i > data.length; i++) {
+            if (data[i] > max) {
                 max = data[i];
-                month = i+1;
+                month = i + 1;
             }
         }
         return max;
     }
-    public int calculateAboveMean(int[] data){
-        float mean = calculateSum(data)/12;
+
+    public int calculateAboveMean(int[] data) {
+        float mean = calculateSum(data) / 12;
         int count = 0;
-        for (int value : data){
-            if ( value > mean){
+        for (int value : data) {
+            if (value > mean) {
                 count = count + 1;
             }
         }
         return count;
     }
-    public int calculateBelowMean(int[] data){
-        float mean = calculateSum(data)/12;
+
+    public int calculateBelowMean(int[] data) {
+        float mean = calculateSum(data) / 12;
         int count = 0;
-        for (int value : data){
-            if ( value < mean){
+        for (int value : data) {
+            if (value < mean) {
                 count = count + 1;
             }
         }
